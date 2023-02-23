950b8c0300cf5bec102dae511a40b7cd7df21d07
njoybro <117508169+njoybro@users.noreply.github.com>
2023-02-23 20:27:37 +0530

    Create filehandling.java

---
+++ filehandling.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.lang.*;
+class Fileread
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			FileReader fin = new FileReader("file1.txt");
+			FileWriter fout = new FileWriter("file2.txt");
+			int i;
+			while((i=fin.read()) != -1)
+			{
+				fout.write(i);
+			}
+			fin.close();
+			fout.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
